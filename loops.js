a0b14e317f9b63443e96717c960332861655ee9a
Lamine <lmesroua@outlook.com>
2022-09-17 13:06:06 -0400

    update js file

--- loops.js
+++ loops.js
@@ -1,65 +1,65 @@
-// 1. Create a loop that logs the numbers from 0-99 (ascending)
-//console.log('Create a loop that logs the numbers from 0-99 (ascending)');
-//for (let i=0; i<=99 ; i++){
-//    console.log(i);
-//    
-//
-// 2. Create a loop that logs the numbers from 99-0 (descending)
-//console.log('Create a loop that logs the numbers from 99-0 (descending)');
-//for (let i=99; i>=0 ; i--){
-//    console.log(i);
-//}
+ //1. Create a loop that logs the numbers from 0-99 (ascending)
+console.log('1. Create a loop that logs the numbers from 0-99 (ascending)');
+for (let i=0; i<=99 ; i++){
+    console.log(i);
+}    
+
+ //2. Create a loop that logs the numbers from 99-0 (descending)
+console.log('2. Create a loop that logs the numbers from 99-0 (descending)');
+for (let i=99; i>=0 ; i--){
+    console.log(i);
+}
 
 //3.Create a loop that logs the EVEN numbers from 0-98 (ascending)
-// console.log('Create a loop that logs the EVEN numbers from 0-98 (ascending)');
-
-//for (let i=2; i <= 98 ; i+=2){
-//    console.log(i);
-//}
+ console.log('3. Create a loop that logs the EVEN numbers from 0-98 (ascending)')
+for (let i=2; i <= 98 ; i+=2){
+    console.log(i);
+}
 
 //4. Create a loop that logs the EVEN numbers from 98-0 (descending)
-//console.log('Create a loop that logs the EVEN numbers from 98-0 (descending');
-//for (let i=98 ; i>=2 ; i-=2){
-//    console.log(i);
-//}
+console.log('4. Create a loop that logs the EVEN numbers from 98-0 (descending)');
+for (let i=98 ; i>=2 ; i-=2){
+    console.log(i);
+};
 
 
 //5. Create a loop that logs the ODD numbers from 0-99 (ascending)
-//console.log('Create a loop that logs the ODD numbers from 0-99 (ascending');
-//for (let i=0 ; i <= 99 ; i++){
-//    if ( i % 2 !=0){
-//        console.log(i);
-//        i++
-//    }   
-//}
-
+console.log('5. Create a loop that logs the ODD numbers from 0-99 (ascending)');
+for (let i=0 ; i <= 99 ; i++){
+    if ( i % 2 !=0){
+        console.log(i);
+        i++
+    };
+}
 //6. Create a loop that logs the ODD numbers from 99-0 (descending)
-//console.log('Create a loop that logs the ODD numbers from 99-0 (descending');
-//
-//for (let i = 99; i >= 0 ; i--){
-//    if(i % 2 != 0){
-//        console.log(i)
-//        i--
-//    }
-//};
+console.log('6. Create a loop that logs the ODD numbers from 99-0 (descending)');
+
+for (let i = 99; i >= 0 ; i--){
+    if(i % 2 != 0){
+        console.log(i)
+        i--
+    }
+}
 
 //7. Create a loop that logs the numbers from 49-72 (ascending)
-//console.log('Create a loop that logs the numbers from 49-72 (ascending');
-//for (i = 49 ; i <=72 ; i++){
-//    console.log(i);
-//}
+console.log('7. Create a loop that logs the numbers from 49-72 (ascending)');
+for (i = 49 ; i <=72 ; i++){
+    console.log(i);
+
+}
+
 
 //8. Create a loop that logs the numbers from 81-26 (descending)
-//console.log('Create a loop that logs the numbers from 81-26 (descending');
-//for (let i = 81 ; i >= 26 ; i--){
-//    console.log(i);
-//}
-//
+console.log('8. Create a loop that logs the numbers from 81-26 (descending)');
+for (let i = 81 ; i >= 26 ; i--){
+    console.log(i);
+}
 
 //.9 Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
-//console.log('Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending');
-//for (i = 3 ; i <= 90 ; i++){
-//    if(i % 3 === 0){
-//        console.log(i)
-//    }
-//}
+console.log('9. Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)');
+
+for(let i = 3 ; i <= 90 ; i++ );{
+    if(i % 3 === 0){
+        console.log(i);
+    }
+};
